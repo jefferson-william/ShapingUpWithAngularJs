d53ab0ce7739cf2e021c00ad2435be9da648a7dd
unknown <Pro06@DEV11.UNI4.local>
2016-05-02 16:31:37 -0300

    Level 1 completed

--- 1/scripts/app.js
+++ 1/scripts/app.js
@@ -1,1 +1,13 @@
-var app;
+(function() {
+    var app = angular.module('gemStore', []);
+
+    app.controller('StoreController', function(){
+        this.products = gems;
+    });
+
+    var gems = [
+        { name: 'Azurite', price: 2.95, canPurchase: true, soldOut: false },
+        { name: 'Bloodstone', price: 5.95, canPurchase: true, soldOut: true },
+        { name: 'Zircon', price: 3.95, canPurchase: false, soldOut: false }
+    ];
+})();
